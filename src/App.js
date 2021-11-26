e5b93aaf375bb02d66e6879f95f081732bfbb8f9
Rishabh Das <rishabhdasufl@gmail.com>
2021-11-26 14:57:09 +0530

    Add react-icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { FcLike } from "react-icons/fc";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        <FcLike />
+        Hello World!
+      </h1>
+    </>
+  );
 }
 
 export default App;
